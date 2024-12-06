a641d9e434a3262e3c842c3106c2dbded1361734
Jareth Gomes <jarethgomes@gmail.com>
2024-12-06 20:51:14 +0000

    Adds part 2 solution

--- src/day02.zig
+++ src/day02.zig
@@ -11,6 +11,7 @@ const gpa = util.gpa;
 pub fn main() !void {
     const data = @embedFile("data/day02.txt");
     print("part 1: {}\n", .{part1(data)});
+    print("part 2: {}\n", .{part2(data)});
 }
 const Direction = enum(u1) {
     increasing,
@@ -24,14 +25,14 @@ fn isSafeSnapshot(currentNumber: u64, prevNumber: ?u64, ordering: ?Direction) bo
         if (ordering) |o| {
             switch (o) {
                 Direction.increasing => {
-                    return (p + minDiff <= currentNumber) and (p + maxDiff >= currentNumber);
+                    return ((p +| minDiff) <= currentNumber) and ((p +| maxDiff) >= currentNumber);
                 },
                 Direction.decreasing => {
-                    return (p - minDiff >= currentNumber) and (p - maxDiff <= currentNumber);
+                    return ((p -| minDiff) >= currentNumber) and ((p -| maxDiff) <= currentNumber);
                 },
             }
         } else {
-            return ((p + minDiff <= currentNumber) and (p + maxDiff >= currentNumber)) or ((p - minDiff >= currentNumber) and (p - maxDiff <= currentNumber));
+            return (((p +| minDiff) <= currentNumber) and ((p +| maxDiff) >= currentNumber)) or (((p -| minDiff) >= currentNumber) and ((p -| maxDiff) <= currentNumber));
         }
     }
     return true;
@@ -71,6 +72,104 @@ fn part1(data: []const u8) u64 {
             }
         }
         if (isSafe) {
+            safeReportCount += 1;
+        }
+    }
+
+    return safeReportCount;
+}
+
+fn part2(data: []const u8) u64 {
+    var lines = std.mem.splitScalar(u8, data, '\n');
+
+    var safeReportCount: u64 = 0;
+    while (lines.next()) |line| {
+        if (std.mem.eql(u8, line, "")) {
+            break;
+        }
+        var numbers = std.mem.splitScalar(u8, line, ' ');
+        var numArr = std.ArrayList(u64).init(std.heap.page_allocator);
+        while (numbers.next()) |num_str| {
+            const num = std.fmt.parseInt(u64, num_str, 10) catch |err| {
+                std.debug.panic("String cannot be parsed as number: \"{s}\" {any}", .{ line, err });
+            };
+            numArr.append(num) catch {
+                @panic("Could not add to num array");
+            };
+        }
+
+        const levelsPerReport: usize = numArr.items.len;
+        const badLevelsAllowed = 2;
+
+        var incMemArr: [badLevelsAllowed][]bool = [badLevelsAllowed][]bool{ &[0]bool{}, &[0]bool{} };
+        var decMemArr: [badLevelsAllowed][]bool = [badLevelsAllowed][]bool{ &[0]bool{}, &[0]bool{} };
+
+        for (0..badLevelsAllowed) |idx| {
+            var row = std.ArrayList(bool).init(std.heap.page_allocator);
+            for (0..levelsPerReport - 1) |_| {
+                row.append(false) catch {
+                    @panic("Could not add to row");
+                };
+            }
+            for (0..2) |_| {
+                row.append(true) catch {
+                    @panic("Could not add to row: true");
+                };
+            }
+
+            incMemArr[idx] = row.items;
+        }
+
+        for (0..badLevelsAllowed) |idx| {
+            var row = std.ArrayList(bool).init(std.heap.page_allocator);
+            for (0..levelsPerReport - 1) |_| {
+                row.append(false) catch {
+                    @panic("Could not add to row");
+                };
+            }
+            for (0..2) |_| {
+                row.append(true) catch {
+                    @panic("Could not add to row: true");
+                };
+            }
+
+            decMemArr[idx] = row.items;
+        }
+
+        // backtrack(currIdx, badLevels, ordering);
+        // if good level
+        // backtrack(currIdx + 1, badLevels, ordering);
+        // if bad level
+        // backtrack(currIdx + 2, badLevels + 1, ordering);
+
+        const numArrSlice = numArr.items;
+        for ([2]Direction{ Direction.increasing, Direction.decreasing }) |direction| {
+            const memArr: *[badLevelsAllowed][]bool = switch (direction) {
+                Direction.increasing => &incMemArr,
+                Direction.decreasing => &decMemArr,
+            };
+
+            var badLevels: usize = 2;
+            while (badLevels > 0) {
+                badLevels -= 1;
+                const len = memArr.*[badLevels].len;
+                var currIdx = len - 2;
+                while (currIdx > 0) {
+                    currIdx -= 1;
+
+                    memArr[badLevels][currIdx] = isSafeSnapshot(numArrSlice[currIdx + 1], numArrSlice[currIdx], direction) and memArr[badLevels][currIdx + 1];
+
+                    if (currIdx + 2 < len and badLevels + 1 < badLevelsAllowed) {
+                        var isSafeJump = true;
+                        if (currIdx + 2 < numArrSlice.len) {
+                            isSafeJump = isSafeSnapshot(numArrSlice[currIdx + 2], numArrSlice[currIdx], direction);
+                        }
+                        memArr[badLevels][currIdx] = memArr[badLevels][currIdx] or (isSafeJump and memArr[badLevels + 1][currIdx + 2]);
+                    }
+                }
+            }
+        }
+        if (incMemArr[0][0] or incMemArr[1][1] or decMemArr[0][0] or decMemArr[1][1]) {
             safeReportCount += 1;
         }
     }
